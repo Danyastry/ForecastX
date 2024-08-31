23fecd8d44aaa54d9953ce5acfe17cf23f656d66
DXD <108821225+Danyastry@users.noreply.github.com>
2024-08-31 18:38:27 +0200

    1.10.1

--- app/src/main/java/com/example/weatherapp/DailyForecast/Forecast.kt
+++ app/src/main/java/com/example/weatherapp/DailyForecast/Forecast.kt
@@ -3,4 +3,5 @@ package com.example.weatherapp.DailyForecast
 data class Forecast(
     val forecastday: List<Forecastday>,
     val forecastday1: List<Forecastday>,
+    val forecastday2: List<Forecastday>,
 )
